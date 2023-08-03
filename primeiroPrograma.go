9a5f57b0b0355cdf82e1f3a193fe888dc7bf1643
Pedro Oliveira <pedro.dev.oliveira@gmail.com>
2023-08-03 14:56:22 -0300

    :sparkles: os and net/http libs and infinity for

--- primeiroPrograma.go
+++ primeiroPrograma.go
@@ -1,31 +1,36 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	showIntroduction()
-	var command int = readCommand()
-	//if comando == 1 {
-	//	fmt.Println("Monitorando")
-	//} else if comando == 2 {
-	//	fmt.Println("Exibindo Logs")
-	//} else if comando == 0 {
-	//	fmt.Println("Saindo do programa")
-	//} else {
-	//	fmt.Println("Comando não reconhecido")
-	//}
+	for {
+		showIntroduction()
+		var command int = readCommand()
+		//if comando == 1 {
+		//	fmt.Println("Monitorando")
+		//} else if comando == 2 {
+		//	fmt.Println("Exibindo Logs")
+		//} else if comando == 0 {
+		//	fmt.Println("Saindo do programa")
+		//} else {
+		//	fmt.Println("Comando não reconhecido")
+		//}
 
-	switch command {
-	case 1:
-		fmt.Println("Monitorando")
-	case 2:
-		fmt.Println("Exibindo Logs")
-	case 0:
-		os.Exit(0)
-	default:
-		fmt.Println("Comando não reconhecido")
-		os.Exit(-1)
+		switch command {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Exibindo Logs")
+		case 0:
+			os.Exit(0)
+		default:
+			fmt.Println("Comando não reconhecido")
+			os.Exit(-1)
+		}
 	}
 
 }
@@ -40,6 +45,19 @@ func readCommand() int {
 	var command int
 	fmt.Scan(&command)
 	// fmt.Println("O endereço de memória da minha variável comando é: ", &command)
-	fmt.Println("O comando escolhido foi: ", command)
+	fmt.Println("O comando escolhido foi:", command)
 	return command
+}
+
+func startMonitoring() {
+	fmt.Println("Monitorando")
+	var url string = "https://alura.com.br"
+	var res, _ = http.Get(url)
+
+	if res.StatusCode == 200 {
+		fmt.Println("Site:", url, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", url, "está com problemas")
+	}
+
 }
